8d119989f8f536d94746ba120a86628234bd767d
Antoinette Lee <43924662+Nyette@users.noreply.github.com>
2021-02-25 03:27:08 -0500

    Added options via argparse

--- valheim_backup_manager_cli.py
+++ valheim_backup_manager_cli.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 import platform
 import schedule
@@ -5,43 +6,48 @@ import shutil
 import sys
 import time
 
-def backup():
-    config = {
+def backup(config):
+    location = {
         'Windows': Path.home() / 'AppData' / 'LocalLow' / 'IronGate' / 'Valheim'
     }
     current_os = platform.system()
-    src = config[current_os]
+    src = location[current_os]
     if src.exists():
         worlds_path = src / 'worlds'
         if worlds_path.exists():
             paths = list(worlds_path.iterdir())
             if len(paths) > 0:
-                big_db_paths = [path for path in paths if str(path).endswith('.db') and path.stat().st_size >= 1000]
+                big_db_paths = [path for path in paths if str(path).endswith('.mp4') and path.stat().st_size >= 1000]
                 if len(big_db_paths) > 0:
-                    dst = Path.home() / 'valheim_backup'
-                    shutil.copytree(src, dst, dirs_exist_ok = True)
+                    dst = config['dst'] / 'valheim_backup'
+                    shutil.copytree(src, dst, dirs_exist_ok=True)
                     print(f"Copied data at {src} to {dst}")
                     time_stamp = time.ctime()
                     print(f"Created backup on {time_stamp}")
                     print("Press [Ctrl] and [C] to stop backup and exit\n")
-                    return 0
+                    config['status'] = 0
+                    return config
                 else:
                     print("Your world db files are either missing or not yet big enough to backup")
-                    return 1
+                    config['status'] = 1
+                    return config
             else:
                 print("The worlds directory is empty")
-                return 1
+                config['status'] = 1
+                return config
         else:
             print("The worlds directory does not exist")
-            return 1
+            config['status'] = 1
+            return config
     else:
         print("Install Valheim first")
-        return 1
+        config['status'] = 1
+        return config
 
-def main():
-    status = backup()
-    if status == 0:
-        schedule.every(5).minutes.do(backup)
+def main(config):
+    config = backup(config)
+    if config['status'] == 0:
+        schedule.every(config['freq']).minutes.do(backup, config)
         try:
             while True:
                 schedule.run_pending()
@@ -53,5 +59,26 @@ def main():
         input("Press [Enter] to exit\n")
         sys.exit()
 
+def handle_input():
+    parser = argparse.ArgumentParser(
+        description="Automatically backup your Valheim data"
+    )
+    parser.add_argument(
+        "--dst",
+        type=Path,
+        default=Path.home(),
+        help="This is a string representing the path you'd like to save the backup to.",
+    )
+    parser.add_argument(
+        "--freq",
+        type=int,
+        default=5,
+        help="This is an integer representing how often the backup should happen in minutes.",
+    )
+    args = parser.parse_args()
+    config = vars(args)
+    return config
+
 if __name__ == '__main__':
-    main()
+    config = handle_input()
+    main(config)
